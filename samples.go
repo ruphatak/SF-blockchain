94d5631974bf1c84ef09ff7d5d09e842b9b5d74f
ruphatak <rucha.a.phatak@capgemini.com>
2017-07-14 17:41:48 +0530

    Add files via upload

---
+++ samples.go
@@ -0,0 +1,44 @@
+package main
+
+var samples = `
+{
+    "event": {
+		"farmID": "SF001",
+		"farmerid": "FMR001",
+		"watersalinity": "40",
+		"soilph": "5",
+		"soilmoisture": "50",
+		"nitrogen": "3",
+		"potassium": "2.342",
+		"sulphur": "1.456",
+		"longitude": "123.456",
+		"luminosity": "28",
+		"ambienttemperature": "39",
+		"ambienthumidity": "1.4",
+		"soiltemperature": "20",
+		"latiude": "4.0",
+		"time": "11234567890123"
+		
+    },
+    "initEvent": {
+        "nickname": "SIMPLE",
+        "version": "The ID of a managed asset. The resource focal point for a smart contract."
+    },
+    "state": {
+			"farmID": "SF001",
+		"farmerid": "FMR001",
+		"watersalinity": "40",
+		"soilph": "5",
+		"soilmoisture": "50",
+		"nitrogen": "3",
+		"potassium": "2.342",
+		"sulphur": "1.456",
+		"longitude": "123.456",
+		"luminosity": "28",
+		"ambienttemperature": "39",
+		"ambienthumidity": "1.4",
+		"soiltemperature": "20",
+		"latitude": "-4.0",
+		"time": "11234567890123"
+    }
+}`
